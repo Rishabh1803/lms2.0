054d3763196a20281ed4a491c71bcc91c0c8e500
Rishabh1803 <rishabhkhare18@gmail.com>
2021-11-20 13:47:19 +0530

    Adding Get Mapping

--- EmployeeDataJPA/src/main/java/com/sujata/controllers/EmployeeController.java
+++ EmployeeDataJPA/src/main/java/com/sujata/controllers/EmployeeController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,7 +27,7 @@ public class EmployeeController {
 //		return new ModelAndView("index");
 //	}
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String openingPageController() {
 		return "index";
 	}
